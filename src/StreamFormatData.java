b5cf5be2d33634aa920751cf7f5c8976c56aa97e
Divyansh Malvi <divymalvi2005@gmail.com>
2024-12-03 19:53:47 +0530

    Discrypted Output

--- src/StreamFormatData.java
+++ src/StreamFormatData.java
@@ -7,11 +7,11 @@ public class StreamFormatData {
             FileOutputStream fos = new FileOutputStream(f);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
             DataOutputStream dos = new DataOutputStream(bos);
-            dos.writeChars("Hello World");
+            dos.writeUTF("Hello World");
             dos.writeDouble(87.5);
             dos.writeInt(4561);
             dos.writeBoolean(false);
-            dos.writeChars("Process Finished");
+            dos.writeUTF("Process Finished");
             dos.close();
             bos.close();
             fos.close();
@@ -23,10 +23,21 @@ public class StreamFormatData {
             FileInputStream fis = new FileInputStream(f);
             BufferedInputStream bis = new BufferedInputStream(fis);
             DataInputStream dis = new DataInputStream(bis);
-            String st;
-            while ((st=dis.readLine()) != null){
-                System.out.println(st);
-            }
+//            String st;
+//            while ((st=dis.readLine()) != null){
+//                System.out.println(st);
+//            }
+            String s = dis.readUTF();
+            double d1 = dis.readDouble();
+            int i = dis.readInt();
+            boolean b = dis.readBoolean();
+            String s1 = dis.readUTF();
+            System.out.println(s);
+            System.out.println("d1 = " + d1);
+            System.out.println("i = " + i);
+            System.out.println("b = " + b);
+            System.out.println("s1 = " + s1);
+
             dis.close();
             bis.close();
             fis.close();
